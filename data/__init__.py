194583381c7e02118b5dc410fa3c9a33335ae5a4
lj-universityOfSurrey <83965695+lj-universityOfSurrey@users.noreply.github.com>
2022-08-05 16:43:20 +0100

    Add files via upload

--- data/__init__.py
+++ data/__init__.py
@@ -1,1 +1,3 @@
-
+from .wider_face import WiderFaceDetection, detection_collate
+from .data_augment import *
+from .config import *
---
+++ data/config.py
@@ -0,0 +1,23 @@
+# config.py
+
+cfg_re50 = {
+    'name': 'Resnet50',
+    'min_sizes': [[16, 32], [64, 128], [256, 512]],
+    'steps': [8, 16, 32],
+    'variance': [0.1, 0.2],
+    'clip': False,
+    'loc_weight': 2.0,
+    'gpu_train': True,
+    'batch_size':40,
+    'ngpu':3,
+    'epoch': 200,
+    'decay1': 80,
+    'decay2': 120,
+    'decay3': 150,
+    'image_size': 800,
+    'pretrain': True,
+    'return_layers': {'layer2': 1, 'layer3': 2, 'layer4': 3},
+    'in_channel': 256,
+    'out_channel': 256
+}
+
